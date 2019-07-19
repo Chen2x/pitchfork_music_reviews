b28135f9619992236f7823a9f7e5b5d25c0e09db
Chen Chen <chenchen199727@gmail.com>
2019-07-19 17:46:15 -0400

    left join reviews with genres

--- Code/top_artist_average.py
+++ Code/top_artist_average.py
@@ -15,14 +15,14 @@ def multipleScore(releases):
 	query = "SELECT reviewid, artist, AVG(score) as avgscore, COUNT(artist) as releases FROM reviews GROUP BY artist HAVING COUNT(artist) > {} ORDER BY AVG(score) DESC LIMIT 10".format(releases)
 	multartists = pd.read_sql_query(query, conn)
 
-	print(multartists)
-
-multipleScore(3)
+	#print(multartists)
 
 
-#def topArtistinGenre(genre):
-#	with
-	#genre10 = pd.read_sql_query("SELECT reviewid, artist, AVG(score) as avgscore, genre, COUNT(artist) as releases FROM reviews WHERE genre = 'rock' GROUP BY artist HAVING COUNT(artist)>3 ORDER BY AVG(score) DESC LIMIT 10 ", conn)
-    #print(genre10)
 
+
+def topArtistsinGenre(genre):
+	withGenre = pd.read_sql_query("SELECT reviewid, title, artist, score, genre FROM reviews LEFT JOIN genres USING(reviewid) AND (genre == {}) ORDER BY score DESC LIMIT 10".format(genre), conn)
+	print(withGenre)
+
+topArtistsinGenre("rock")
     #return reviews.loc[(reviews.score >= score) & (reviews.artist == artist.lower())]
