eeecbf0a3c0c96c77c7fd497afa4ef93f90a3012
Chen Chen <chenchen199727@gmail.com>
2019-07-22 15:24:57 -0400

    CLI --help implemented

--- Code/Pitchfork_music_reviews.py
+++ Code/Pitchfork_music_reviews.py
@@ -8,15 +8,25 @@ from Pitchfork_artist_score_vis import artistScoreTimeVis, avgArtistScoreVis
 
 @click.command()
 @click.argument('function')
-@click.option('--releases')
-@click.option('--genre')
-@click.option('--amount', default = 10)
-@click.option('--score')
-@click.option('--artist')
-@click.option('--minreview')
-@click.option('--name')
-@click.option('--topnumber')
-def main(function, releases, genre, amount, score, artist, minreview, name, topnumber):
+@click.option('--releases', default=1, help="minimum releases by an artist")
+@click.option('--genre', help="genre to search in")
+@click.option('--amount', default = 10, help="amount of results to be shown")
+@click.option('--score', default = 0, help="minimum score or albums to be displayed")
+@click.option('--artist', help="artist name as string")
+@click.option('--minreview', default = 1, help="minimum reviews by an author")
+def main(function, releases, genre, amount, score, artist, minreview, name, topnumber, help):
+	"""
+	Command line interface for entering query parameters 
+
+	possible querys:
+
+	multipleScore(releases, amount) \n
+	topArtistsinGenre(genre, amount) \n
+	artistScoreFilter(score, artist) \n
+	avgReviewerScore(minreview, amount) \n
+	artistScoreTimeVis(artist) \n
+	avgArtistScoreVis(releases, amount)
+	"""
 	if function == "multipleScore":
 		click.echo(multipleScore(releases, amount))
 	if function == "topArtistsinGenre":
@@ -26,9 +36,10 @@ def main(function, releases, genre, amount, score, artist, minreview, name, topn
 	if function == "avgReviewerScore":
 		click.echo(avgReviewerScore(minreview, amount))
 	if function == "artistScoreTimeVis":
-		click.echo(artistScoreTimeVis(name))
+		click.echo(artistScoreTimeVis(artist))
 	if function == "avgArtistScoreVis":
-		click.echo(avgArtistScoreVis(releases, topnumber))
+		click.echo(avgArtistScoreVis(releases, amount))
+
 
 if __name__ == '__main__':
 	main()
